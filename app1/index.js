04be37b8f87bd44c456e08e6610983320d678a90
harshjmhr <harshjmhr@gmail.com>
2023-07-16 23:10:21 +0530

    update changes

--- app1/index.js
+++ app1/index.js
@@ -1,34 +1,58 @@
 const express = require("express");
-const kafak = require("kafka-node");
+//const kafak = require("kafka-node");
+const { Kafka } = require("kafkajs");
+
 const app = express();
 app.use(express.json());
 
-const start = async () => {
-  const client = new kafak.KafkaClient({ kafkaHost: "kafka:9092" });
-  const producer = new kafak.Producer(client);
-  producer.on("ready", async () => {
-    app.post("/", async (req, res) => {
-        try{
-            producer.send(
-                [{ topic: "test", message: JSON.stringify(req.body) }],
-                async (err, data) => {
-                  if (err) console.log(err);
-                  else {
-                    console.log(data);
-                    console.log("data sent");
-                    res.status = 200;
-                    res.send("data sent");
-                  }
-                }
-              );
-        } catch(e) {
-            console.log(`error: ${e}`)
-        }
-      
+const kafka = new Kafka({
+  clientId: "my-app",
+  brokers: ["kafka:9092"],
+});
 
+app.post("/", async (req, res) => {
+  try {
+    const producer = kafka.producer();
+
+    await producer.connect();
+    await producer.send({
+      topic: "test",
+      messages: [{ value: JSON.stringify(req.body) }],
     });
-  });
-};
 
-setTimeout(start, 1000);
+    await producer.disconnect();
+    res.status = 200;
+    res.send("data sent");
+  } catch (e) {
+    console.log(`error: ${e}`);
+  }
+});
+
+// const start = async () => {
+//   const client = new kafak.KafkaClient({ kafkaHost: "kafka:9092" });
+//   const producer = new kafak.Producer(client);
+//   producer.on("ready", async () => {
+//     app.post("/", async (req, res) => {
+//         try{
+//             producer.send(
+//                 [{ topic: "test", message: JSON.stringify(req.body) }],
+//                 async (err, data) => {
+//                   if (err) console.log(err);
+//                   else {
+//                     console.log(data);
+//                     console.log("data sent");
+//                     res.status = 200;
+//                     res.send("data sent");
+//                   }
+//                 }
+//               );
+//         } catch(e) {
+//             console.log(`error: ${e}`)
+//         }
+
+//     });
+//   });
+// };
+
+//setTimeout(start, 1000);
 app.listen(8080);
--- app2/index.js
+++ app2/index.js
@@ -1,23 +1,49 @@
 const express = require("express");
-const kafak = require("kafka-node");
+//const kafak = require("kafka-node");
+const { Kafka } = require('kafkajs');
 const app = express();
 app.use(express.json());
 
-const start = async () => {
-  const client = new kafak.KafkaClient({ kafkaHost: "kafka:9092" });
-  const consumer = new kafak.Consumer(client, [{ topic: "test" }], {
-    autoCommit: false,
+const kafka = new Kafka({
+    clientId: "my-app",
+    brokers: ["kafka:9092"],
   });
-  consumer.on("message", async (message) => {
-    console.log("here we recived event on message");
-    console.log("---------------------------------");
-    console.log(message);
-    console.log("---------------------------------");
-  });
-  consumer.on("error", async (error) => {
-    console.log(error);
-  });
-};
+
+  start = async() =>{
+    const consumer = kafka.consumer({ groupId: 'test-group' })
+
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'test', fromBeginning: false })
+    
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+          console.log("consuming------------->>>>");
+        if(message.value)console.log({
+          value: message.value.toString(),
+        })
+      },
+    })
+  }
+
+
+
+// const start = async () => {
+//   const client = new kafak.KafkaClient({ kafkaHost: "kafka:9092" });
+//   const consumer = new kafak.Consumer(client, [{ topic: "test" }], {
+//     autoCommit: false,
+//   });
+//   consumer.on("message", async (message) => {
+//     console.log("here we recived event on message");
+//     console.log("---------------------------------");
+//     console.log(message);
+//     console.log("---------------------------------");
+//   });
+//   consumer.on("error", async (error) => {
+//     console.log(error);
+//   });
+// };
+
+
 
 setTimeout(start, 1000);
 app.listen(8080);
